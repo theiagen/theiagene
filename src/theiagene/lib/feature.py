6d6abc2508d32598c3b722b376d24f3e60eae917
xonq <konkelzach@tuta.io>
2026-07-31 19:18:14 +0000

    add comment

--- src/theiagene/lib/feature.py
+++ src/theiagene/lib/feature.py
@@ -219,6 +219,7 @@ def group_features(features: list, parent_ids: list = ["Parent", "parent"], ids:
         fid = _attr_get(feature.attributes, ids)
         if fid in id2feature:
             forbidden = forbidden.add(fid)
+            # make the ID unique by appending a count
             while fid in id2feature:
                 id_counts[fid] += 1
                 fid = f"{fid}_{id_counts[fid]}"
